597628f84ed43903cd03b8b3c77093be5ed24f3d
mahdi moradi <mm2129260@gmail.com>
2024-02-22 21:57:53 +0330

    Start eshop project

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('<int:pk>', views.BlogDetailView.as_view(), name="blog_detail")
+]
---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# from django.views.generic import DetailView
+#
+#
+# class BlogDetail(DetailView):
+#     template_name = "blog/blog.html"
